bcdd8a7558c2019a0040e1e66430a2c65c0b0794
VitalyChopik <leo.space97@gmail.com>
2023-12-20 14:44:32 +0300

    fix: desktop style

--- src/js/section/form.js
+++ src/js/section/form.js
@@ -2,10 +2,31 @@ const formBlock = (formBlock) => {
   const spinner = formBlock.querySelector('.spinner');
   const form = formBlock.querySelector('.contacts__form-content');
   const formSubmit = form.querySelector('.contacts__form-button');
+  const inputsBox = form.querySelectorAll('.contacts__form-box');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    sendData();
+    if (validateInputs(inputsBox)) {
+      sendData();
+    }
   });
+  function validateInputs(inputsBox) {
+    let isValid = true;
+
+    inputsBox.forEach((box) => {
+      const requiredInputs = box.querySelectorAll('.required');
+
+      requiredInputs.forEach((input) => {
+        if (input.value.trim() === '') {
+          isValid = false;
+          box.classList.add('error');
+        } else {
+          box.classList.remove('error');
+        }
+      });
+    });
+
+    return isValid;
+  }
   function sendData() {
     spinner.classList.add('active');
     const fileInput = form.querySelector('.contacts__form-file-hidden');
@@ -17,7 +38,7 @@ const formBlock = (formBlock) => {
         formData.append('attachment', files[0]);
       }
     }
-
+    // fetch('https://formsubmit.co/support@zilingial.com', {
     fetch('https://formsubmit.co/support@zilingial.com', {
       method: 'POST',
       body: formData,
